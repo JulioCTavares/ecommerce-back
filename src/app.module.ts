d4e140e22f82296d638d36759fba6ae88e54ead3
Julio Tavares <jctbs@cin.ufpe.br>
2023-04-04 11:10:19 -0300

    feat: :sparkles: add docker e typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      port: Number(process.env.DATABASE_PORT),
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto {
+  name: string;
+
+  email: string;
+
+  phone: string;
+
+  cpf: string;
+
+  password: string;
+}
